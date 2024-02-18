70ab9cb6f5963320b321f4a863eedddecf1151e2
HutchyBean <38333275+HutchyBean@users.noreply.github.com>
2024-02-18 16:06:01 +0000

    NO code completion. NO error checking.

--- src/hooks/VoiceStatus.cpp
+++ src/hooks/VoiceStatus.cpp
@@ -8,7 +8,7 @@ namespace hooked_methods
 
 DEFINE_HOOKED_METHOD(VoiceStatus, void, void *this_, int entindex, qboolean bTalking )
 {
-   if (entindex == g_pLocalPlayer->entity_idx || entindex == -2)
+   if (entindex == g_pLocalPlayer->entity_idx || entindex == -2) {
        original::VoiceStatus(this_, entindex, true);
        return;
    }
